ad52299a7183bd67f40c7d509255db59742ab5a0
karenbon221 <karenbon221@gmail.com>
2024-05-13 13:42:17 +0200

    Create arenabot.lua

---
+++ arenabot.lua
@@ -0,0 +1,25 @@
+-- Initializing global variables to store the latest game state and game host process.
+LatestGameState = LatestGameState or nil
+Game = Game or nil
+InAction = InAction or false
+
+Logs = Logs or {}
+
+colors = {
+  red = "\27[31m",
+  green = "\27[32m",
+  blue = "\27[34m",
+  reset = "\27[0m",
+  gray = "\27[90m"
+}
+
+function addLog(msg, text) -- Function definition commented for performance, can be used for debugging
+  Logs[msg] = Logs[msg] or {}
+  table.insert(Logs[msg], text)
+end
+
+-- Checks if two points are within a given range.
+-- @param x1, y1: Coordinates of the first point.
+-- @param x2, y2: Coordinates of the second point.
+-- @param range: The maximum allowed distance between the points.
+-- @return: Boolean indicating if the points are within the specified range.
